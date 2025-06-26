d82fee9a215d179c54d08ef0c7ffc13a9558bdb4
Piyush Kumar Bharti <contact.piyushkumarbharti@gmail.com>
2025-06-26 18:35:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,20 +19,3 @@ def get_config():
         "experiment_name": "runs/tmodel",
     }
 
-
-def get_weights_file_path(config, epoch: str):
-    """docstring"""
-    model_folder = f"{config['datasource']}_{config['model_folder']}"
-    model_filename = f"{config['model_basename']}{epoch}.pt"
-    return str(Path(".") / model_folder / model_filename)
-
-
-def latest_weights_file_path(config):
-    """docstring"""
-    model_folder = f"{config['datasource']}_{config['model_folder']}"
-    model_filename = f"{config['model_basename']}*"
-    weights_files = list(Path(model_folder).glob(model_filename))
-    if len(weights_files) == 0:
-        return None
-    weights_files.sort()
-    return str(weights_files[-1])
