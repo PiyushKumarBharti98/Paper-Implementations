6eb8ea5278967161c139223821e96ba632abb765
PiyushKumarBharti98 <contact.piyushkumarbharti@gmail.com>
2025-08-13 18:25:10 +0530

    okay

---
+++ vision-transformers/model.py
@@ -0,0 +1,15 @@
+import math
+import torch
+from torch import nn
+
+
+class ViT(nn.Module):
+    """docstring"""
+
+    def __init__(
+        self,
+    ) -> None:
+        super().__init__()
+
+    def forward(self, x):
+        return x
