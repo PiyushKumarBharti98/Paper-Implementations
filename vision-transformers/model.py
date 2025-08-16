dbeea83e39f310569f8428d231d433c200dfae51
PiyushKumarBharti98 <contact.piyushkumarbharti@gmail.com>
2025-08-17 03:02:27 +0530

    okay

--- vision-transformers/model.py
+++ vision-transformers/model.py
@@ -1,7 +1,6 @@
 import math
-from typing import final
 import torch
-from torch import batch_norm, embedding, nn
+from torch import nn
 
 
 class PatchEmbeddings(nn.Module):
@@ -34,3 +33,119 @@ class PatchEmbeddings(nn.Module):
         embeddings = torch.cat((cls_tokens, image), dim=1)
         final_embeddings = embeddings + self.patch_embeddings
         return final_embeddings
+
+
+class MultiHeadSelfAttention(nn.Module):
+    """docstring"""
+
+    def __init__(self, embedding_dim: int, num_heads: int, dropout: float) -> None:
+        super().__init__()
+        self.num_heads = num_heads
+        self.head_dim = embedding_dim / num_heads
+        assert self.num_heads * self.head_dim == embedding_dim
+        self.scale = self.head_dim**-0.5
+
+        self.qkv_proj = nn.Linear(embedding_dim, embedding_dim * 3)
+        self.out_proj = nn.Linear(embedding_dim, embedding_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """docstring"""
+        B, N, D = x.shape
+
+        qkv = (
+            self.qkv_proj(x)
+            .reshape(B, N, 3, self.num_heads, self.head_dim)
+            .permute(3, 0, 2, 4, 1)
+        )
+        q, k, v = qkv[0], qkv[1], qkv[2]
+
+        attention = q @ k.transpose(-2, -1) * self.scale
+        attention = attention.softmax(attention)
+        attention = self.dropout(attention)
+
+        weighted_average = attention @ v
+
+        weighted_average = weighted_average.transpose(1, 2).reshape(B, N, D)
+
+        out = self.out_proj(weighted_average)
+
+        return out
+
+
+class MLP(nn.Module):
+    """docstring"""
+
+    def __init__(self, embedding_size: int, mlp_size: int, dropout: float) -> None:
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(embedding_size, mlp_size),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(mlp_size, embedding_size),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """docstring"""
+        return self.net(x)
+
+
+class VITEncoder(nn.Module):
+    """docstring"""
+
+    def __init__(
+        self, embedding_size: int, num_heads: int, mlp_size: int, dropout: float
+    ) -> None:
+        super().__init__()
+        self.norm1 = nn.LayerNorm(embedding_size)
+        self.attention = MultiHeadSelfAttention(embedding_size, num_heads, dropout)
+        self.norm2 = nn.LayerNorm(embedding_size)
+        self.mlp = MLP(embedding_size, mlp_size, dropout)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """docstring"""
+        x += self.attention(self.norm1(x))
+        x += self.mlp(self.norm2(x))
+        return x
+
+
+class FinalModel(nn.Module):
+    """docstring"""
+
+    def __init__(
+        self,
+        image_size: int,
+        patch_size: int,
+        embedding_size: int,
+        in_channels: int,
+        num_heads: int,
+        dropout: float,
+        mlp_size: int,
+        num_layers: int,
+        num_classes: int,
+    ) -> None:
+        super().__init__()
+        self.patch_embeddings = PatchEmbeddings(
+            image_size, patch_size, in_channels, embedding_size
+        )
+        self.encoder = nn.Sequential(
+            *[
+                VITEncoder(embedding_size, num_heads, mlp_size, dropout)
+                for _ in range(num_layers)
+            ]
+        )
+
+        self.norm = nn.LayerNorm(embedding_size)
+        self.classifier = nn.Linear(embedding_size, num_classes)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """docstring"""
+        x = self.patch_embeddings(x)
+        x = self.encoder(x)
+        cls_token_output = x[:, 0]
+        cls_token_output = self.norm(cls_token_output)
+
+        logits = self.classifier(cls_token_output)
+
+        return logits
